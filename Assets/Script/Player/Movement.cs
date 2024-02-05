d96ec40070b152ca1c6b3509118363b43847f475
Neko <90067903+tokin891@users.noreply.github.com>
2024-02-05 07:17:27 +0100

    Basic Move

---
+++ Assets/Script/Player/Movement.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Movement : MonoBehaviour
+{
+    [SerializeField] private float speed = 10.0f;
+    [SerializeField] private float sensMouse = 100f;
+    private Rigidbody rb;
+    private Vector3 movement;
+    [SerializeField] private Camera cam;
+
+    private float xRot = 0;
+    private float yRot = 0;
+
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+        Look(new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y")) * Time.deltaTime * sensMouse);
+    }
+
+    void FixedUpdate()
+    {
+        MoveCharacter(movement);
+    }
+
+    void MoveCharacter(Vector3 direction)
+    {
+        rb.velocity = direction * speed;
+    }
+
+    void Look(Vector2 input)
+    {
+        yRot += input.x;
+        xRot -= input.y;
+        xRot = Mathf.Clamp(xRot, -90, 90);
+
+        cam.transform.rotation = Quaternion.Euler(xRot, yRot, 0);
+        transform.rotation = Quaternion.Euler(0, yRot, 0);
+    }
+}
