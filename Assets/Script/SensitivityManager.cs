24beec48614c92474871cc469b1c367f4c58fcaf
Tomasz <tomie.kai.wab1@gmail.com>
2024-02-08 17:39:05 +0100

    Add Menu

---
+++ Assets/Script/SensitivityManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SensitivityManager : MonoBehaviour
+{
+    public Slider sensitivitySlider;
+
+    private static SensitivityManager instance;
+
+    private void Start(){
+        if (instance == null){
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+            float savedSensitivity = PlayerPrefs.GetFloat("Sensitivity", 1.0f);
+            SetSensitivity(savedSensitivity);
+            SetSliderValue(savedSensitivity);
+
+        } else{
+            Destroy(gameObject);
+            return;
+        }
+    }
+
+    public void SetSensitivity(float sensitivity){
+        PlayerPrefs.SetFloat("Sensitivity", sensitivity);
+        PlayerPrefs.Save();
+    }
+
+    public void SetSliderValue(float value){
+        sensitivitySlider.value = value;
+    }
+}
---
+++ Assets/Script/VolumeManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeManager : MonoBehaviour
+{
+    public Slider volumeSlider;
+    private static VolumeManager instance;
+
+    private void Start() {
+        if (instance == null) {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+            float savedVolume = PlayerPrefs.GetFloat("Volume", AudioListener.volume);
+            SetVolume(savedVolume);
+            SetSliderValue(savedVolume);
+        }
+        else {
+            Destroy(gameObject);
+            return;
+        }
+    }
+
+    public void SetVolume(float volume) {
+        AudioListener.volume = volume;
+        PlayerPrefs.SetFloat("Volume", volume);
+        PlayerPrefs.Save();
+    }
+
+    public void SetSliderValue(float value) {
+        volumeSlider.value = value;
+    }
+}
